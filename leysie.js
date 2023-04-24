05d66869cf6ff94a2f434ccf517ce6238a17f7e4
LeysieAyleen <129077014+LeysieAyleen@users.noreply.github.com>
2023-04-24 09:25:25 -0500

    main

--- leysie.js
+++ leysie.js
@@ -2,4 +2,5 @@ var celsius1 = prompt("Introduzca la temperatura en grados celsius: ");
 celsius1 = parseFloat(celsius1);
 
 //
-var farenheit = (9/5*celsious1+32)
+var farenheit = (9/5*celsious1+32);
+document.write("De celsius a farenheit es: ");
